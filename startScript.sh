56bfb5cb6eb6d2ef9922b1ce8c60c3b7700bc27e
vaspoz <vaspoz@mail.ru>
2022-04-13 11:22:54 +0200

    Forgot to export env variables

--- startScript.sh
+++ startScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "Enter region [eu-central-1]" region
-REGION=${region:-eu-central-1}
+read -p "Enter region [eu-central-1]: " region
+export REGION=${region:-eu-central-1}
 
-read -p "Enter AWS profile to use [default]" profile
-AWSPROFILE=${profile:-default}
+read -p "Enter AWS profile to use [default]: " profile
+export AWSPROFILE=${profile:-default}
 
 echo 
 echo "Installing node modules..."
